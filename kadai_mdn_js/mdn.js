52333c09a0d3338467b606a0ce9ef3b95c5c48e6
happy.n5sweet <happy.n5sweet@gmail.com>
2025-06-26 22:01:09 +0900

    add kadai_mdn

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+const date1 = new Date()
+
+const date = date1.getDate();
+//console.log(date);
+
+const year = date1.getFullYear();
+//console.log(year);
+
+const month = date1.getMonth()+1;
+
+//console.log(month+1);
+
+console.log(year+'年'+month+'月'+date+'日');
